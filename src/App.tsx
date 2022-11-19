cca321b26be0213b54841f2b5ed255b252ea985f
julu1994 <mrjewel837@gmail.com>
2022-11-19 02:00:07 +0100

    initialzed react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
-    return <div className="App">Hello world</div>;
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
