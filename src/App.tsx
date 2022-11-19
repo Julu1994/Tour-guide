433c96b8a65fa6d98e90573c2a961c873f15703a
julu1994 <mrjewel837@gmail.com>
2022-11-19 10:46:54 +0100

    added the navbar basic skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 
 function App() {
     return (
         <div className="App">
             <Router>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                 </Routes>
--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
-
-const index = () => {
-    return <div>index</div>;
+import "./index.scss";
+import { FaArtstation } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-logo">
+                    <FaArtstation />
+                </div>
+                <div className="navbar-menu">
+                    <GiHamburgerMenu />
+                </div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/services">Services</Link>
+                    </li>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/events">Contacts</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
-export default index;
+export default Navbar;
