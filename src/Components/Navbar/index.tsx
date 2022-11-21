bac21f9b7a247b0bd1b6369f6f039605005851eb
julu1994 <mrjewel837@gmail.com>
2022-11-21 02:13:22 +0100

    completed the navbar

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,45 +1,71 @@
 import React from "react";
 import "./index.scss";
 import { FaArtstation } from "react-icons/fa";
+import { TiTimes } from "react-icons/ti";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import { Button } from "../Common/button";
+
 const Navbar = () => {
-    const a = () => {
-        console.log("hi");
+    const [toggle, setToggle] = React.useState(false);
+    const handleToggle = () => {
+        setToggle(!toggle);
     };
+    const handleClose = () => {
+        setToggle(false);
+    };
+    const screenSize = () => {
+        return window.innerWidth > 960;
+    };
+
     return (
         <nav className="navbar">
-            <div className="navbar-container">
-                <div className="navbar-logo">
-                    <FaArtstation />
-                </div>
-                <div className="navbar-menu">
-                    <GiHamburgerMenu />
-                </div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/services">Services</Link>
-                    </li>
-                    <li>
-                        <Link to="/signup">Signup</Link>
-                    </li>
-                    <li>
-                        <Link to="/events">Contacts</Link>
-                    </li>
-                    <Button
-                        onclick={a}
-                        size={"h"}
-                        style={""}
-                        type={""}
-                        children={"Signup"}
-                        path={"/"}
-                    />
-                </ul>
+            <div className="navbar-logo">
+                <FaArtstation size={30} />
+                <h4 className="navbar-text">Travel Wizard</h4>
             </div>
+            <ul className={!toggle ? "navbar-list" : "navbar-list-mobile"}>
+                <li className="navbar-item">
+                    <Link
+                        onClick={handleClose}
+                        className="navbar-links "
+                        to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link
+                        onClick={handleClose}
+                        className="navbar-links"
+                        to="/services">
+                        Services
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link
+                        onClick={handleClose}
+                        className="navbar-links"
+                        to="/events">
+                        Contacts
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link
+                        onClick={handleClose}
+                        className="navbar-links"
+                        to="/signup">
+                        Signup
+                    </Link>
+                </li>
+            </ul>
+            {!screenSize() && (
+                <div className="navbar-icon" onClick={handleToggle}>
+                    {toggle ? (
+                        <TiTimes size={45} />
+                    ) : (
+                        <GiHamburgerMenu size={30} />
+                    )}
+                </div>
+            )}
         </nav>
     );
 };
