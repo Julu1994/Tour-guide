275c6f585612ded91a42cf3a42ff5b8dcd3e2f76
julu1994 <mrjewel837@gmail.com>
2022-11-25 14:30:06 +0100

    completed the signup page

--- src/Components/Common/input.tsx
+++ src/Components/Common/input.tsx
@@ -3,14 +3,15 @@ import "./input.scss";
 type props = {
     cls?: string;
     placeHolder?: string;
+    typ?: string;
 };
-const Input = ({ cls, placeHolder }: props) => {
+const Input = ({ cls, placeHolder, typ }: props) => {
     return (
         <>
             <input
                 placeholder={placeHolder}
                 className={`input ${cls}`}
-                type="text"
+                type={typ ? typ : "text"}
             />
         </>
     );
--- src/Pages/Signup/index.tsx
+++ src/Pages/Signup/index.tsx
@@ -1,12 +1,22 @@
-import Card from "../../Components/Card";
-import Hero from "../../Components/Hero";
 import bgVideo from "../../Assets/Videos/bgVideo4.mp4";
+import Input from "../../Components/Common/input";
+import "./index.scss";
 
 const Signup = () => {
     return (
-        <div>
-            <Hero video={bgVideo} />
-            <Card />
+        <div className="signup">
+            <video className="signup-video" src={bgVideo} autoPlay loop muted />
+            <form className="signup-form">
+                <h4 className="signup-title">Signup Form</h4>
+                <Input cls="signup-name" placeHolder="Name" />
+                <Input cls="Signup-email" placeHolder="Email" />
+                <Input
+                    cls="signup-password"
+                    placeHolder="Password"
+                    typ="password"
+                />
+                <button className="signup-btn">Signup</button>
+            </form>
         </div>
     );
 };
